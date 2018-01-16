e43bf15d7a3794ce403b106ca38b7910e608f64d
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2018-01-16 11:42:24 -0700

    double added for more advanced calculator

--- src/com/company/WeekendChallenge.java
+++ src/com/company/WeekendChallenge.java
@@ -4,33 +4,52 @@ import java.util.Scanner;
 
 public class WeekendChallenge {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Boolean ask = true;
+       Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input First Number:");
-        String firstNumber = sc.nextLine();
-
-        System.out.println("Input one of the following valid operators: +, -, *, / :");
-        String operator = sc.nextLine();
-
-        System.out.println("Input Second Number:");
-        String secondNumber = sc.nextLine();
-
-        int intFirst = Integer.parseInt(firstNumber);
-        int intSecond = Integer.parseInt(secondNumber);
-        int intResult = 0;
-
-        if(operator.equals("+") || operator.equals("add")){
-            intResult = intFirst + intSecond;
-        } else if (operator.equals("-") || operator.equals("subtract")){
-            intResult = intFirst - intSecond;
-        } else if (operator.equals("*") || operator.equals("multiply")){
-            intResult = intFirst * intSecond;
-        } else if (operator.equals("/") || operator.equals("divide")){
-            intResult = intFirst / intSecond;
+       while(ask){
 
 
-        }
+            System.out.println("Input First Number:");
+            String firstNumber = sc.nextLine();
 
-        System.out.println("Your number is: " + intResult);
+            System.out.println("Input one of the following valid operators: +, -, *, / :");
+            String operator = sc.nextLine();
+
+            System.out.println("Input Second Number:");
+            String secondNumber = sc.nextLine();
+
+            //int intFirst = Integer.parseInt(firstNumber);
+           //int intSecond = Integer.parseInt(secondNumber);
+           // int intResult = 0;
+           //Double allows a decimal to be used to find
+           double intFirst = Double.parseDouble(firstNumber);
+           double intSecond = Double.parseDouble(secondNumber);
+           double intResult = 0.0;
+
+            if (operator.equals("+") || operator.equals("add")) {
+                intResult = intFirst + intSecond;
+            } else if (operator.equals("-") || operator.equals("subtract")) {
+                intResult = intFirst - intSecond;
+            } else if (operator.equals("*") || operator.equals("multiply")) {
+                intResult = intFirst * intSecond;
+            } else if (operator.equals("/") || operator.equals("divide")) {
+                intResult = intFirst / intSecond;
+
+
+            }
+
+            System.out.println("Your number is: " + intResult);
+            System.out.println("Would you like to try again?");
+            String userResponse = sc.nextLine();
+
+            if(userResponse.equalsIgnoreCase("yes") || userResponse.equalsIgnoreCase("y")){
+                ask = true;
+
+            } else {
+                ask = false;
+            }
+        }System.out.println("Have a nice day!");
+
     }
 }
